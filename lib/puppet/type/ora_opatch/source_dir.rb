4fda5f5f6e20d8f1a5eb67b3de2490a164db0e24
Bert Hajee <hajee@moretIA.com>
2015-12-22 16:17:39 +0100

    [ora_opatch] renamed source_folder to source_dir

--- lib/puppet/type/ora_opatch/source_dir.rb
+++ lib/puppet/type/ora_opatch/source_dir.rb
@@ -1,18 +1,19 @@
-newparam(:source_folder) do
+newparam(:source_dir) do
   desc <<-EOT
     The source folder in the zip to use.
 
     This parameter is only valid when you are using a source zip. It describes the folder in the zip to use as a base
-    for running the several Opatch utilities.
+    for running the several Opatch utilities. When you don't specify a value, it
+    uses the patch_id.
 
     example:
 
         ora_opatch{ "/app/grid/product/12.1/grid:21359755":
-          ensure        => present,
+          ensure     => present,
           ...
-          tmp_dir       => '/tmp/patches'
-          source        => '/downloads/p21523260_121020_Linux-x86-64.zip',
-          source_folder => '21523260',
+          tmp_dir    => '/tmp/patches'
+          source     => '/downloads/p21523260_121020_Linux-x86-64.zip',
+          source_dir => '21523260',
         }
 
     This will run the Opatch command on the folder `/tmp/patches/21523260'
@@ -20,9 +21,9 @@ newparam(:source_folder) do
   EOT
 end
 
-def source_folder
-  if self[:source_folder]
-    self[:source_folder]
+def source_dir
+  if self[:source_dir]
+    self[:source_dir]
   else
     self[:patch_id]
   end
